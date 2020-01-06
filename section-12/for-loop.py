4b2a19510a456de84d78a2335a2feeed97ae5bee
André Braithwaite <andrebwt@live.com>
2020-01-06 14:08:25 +0000

    Completed section 13.

--- section-12/for-loop.py
+++ section-12/for-loop.py
@@ -5,6 +5,7 @@ for colour in colours:
 	print(colour)
 
 # Print list with while
+# Useful if you need to use the index for something
 i = 0
 while i < len(colours):
 	print('{}: {}'.format(i, colours[i]))
